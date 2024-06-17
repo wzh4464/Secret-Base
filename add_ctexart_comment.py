36e65aacc2046d8ffccc96deaf68a9b090ab5431
Zihan <32484940+wzh4464@users.noreply.github.com>
2024-06-17 20:03:23 +0800

    1.0.3

--- add_ctexart_comment.py
+++ add_ctexart_comment.py
@@ -6,7 +6,7 @@ import os
  # Created Date: Monday, June 17th 2024
  # Author: Zihan
  # -----
- # Last Modified: Monday, 17th June 2024 7:53:50 pm
+ # Last Modified: Monday, 17th June 2024 7:59:27 pm
  # Modified By: the developer formerly known as Zihan at <wzh4464@gmail.com>
  # -----
  # HISTORY:
@@ -43,14 +43,14 @@ md_files = glob.glob('**/*.md', recursive=True)
 #     f.truncate()
 
 # substitute all space in file name with '_'
-# for file in md_files:
-#   new_file = file.replace(' ', '_')
-#   print(f'Renaming {file} to {new_file}')
-#   os.rename(file, new_file)
+for file in md_files:
+  new_file = file.replace(' ', '_')
+  print(f'Renaming {file} to {new_file}')
+  os.rename(file, new_file)
 
 # also for foler name
-folders = glob.glob('**/', recursive=True)
-for folder in folders:
-  new_folder = folder.replace(' ', '_')
-  print(f'Renaming {folder} to {new_folder}')
-  os.rename(folder, new_folder)
+# folders = glob.glob('**/', recursive=True)
+# for folder in folders:
+#   new_folder = folder.replace(' ', '_')
+#   print(f'Renaming {folder} to {new_folder}')
+#   os.rename(folder, new_folder)
