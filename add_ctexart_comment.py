405c5c7ad2e17dcd7ea8cecece67106435fa6d3f
Zihan <32484940+wzh4464@users.noreply.github.com>
2024-06-17 19:54:35 +0800

    script

--- add_ctexart_comment.py
+++ add_ctexart_comment.py
@@ -1,11 +1,12 @@
 import glob
+import os
 
 ###
  # File: /add_ctexart_comment.py
  # Created Date: Monday, June 17th 2024
  # Author: Zihan
  # -----
- # Last Modified: Monday, 17th June 2024 4:31:23 pm
+ # Last Modified: Monday, 17th June 2024 7:53:50 pm
  # Modified By: the developer formerly known as Zihan at <wzh4464@gmail.com>
  # -----
  # HISTORY:
@@ -17,19 +18,19 @@ import glob
 md_files = glob.glob('**/*.md', recursive=True)
 
 # Comment to be added
-comment = '''---
-toc: true
-documentclass: "ctexart"
-classoption: "UTF8"
----
-'''
+# comment = '''---
+# toc: true
+# documentclass: "ctexart"
+# classoption: "UTF8"
+# ---
+# '''
 
 # Add the comment to each file
-for file in md_files:
-  with open(file, 'r+') as f:
-    content = f.read()
-    f.seek(0, 0)
-    f.write(comment + content)
+# for file in md_files:
+#   with open(file, 'r+') as f:
+#     content = f.read()
+#     f.seek(0, 0)
+#     f.write(comment + content)
 
 # delete first blank lines
 # for file in md_files:
@@ -40,3 +41,16 @@ for file in md_files:
 #       if line.strip() != '':
 #         f.write(line)
 #     f.truncate()
+
+# substitute all space in file name with '_'
+# for file in md_files:
+#   new_file = file.replace(' ', '_')
+#   print(f'Renaming {file} to {new_file}')
+#   os.rename(file, new_file)
+
+# also for foler name
+folders = glob.glob('**/', recursive=True)
+for folder in folders:
+  new_folder = folder.replace(' ', '_')
+  print(f'Renaming {folder} to {new_folder}')
+  os.rename(folder, new_folder)
